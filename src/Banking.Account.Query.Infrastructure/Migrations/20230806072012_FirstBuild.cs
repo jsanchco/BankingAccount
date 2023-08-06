129724e2400173315916fa5b225816fd2921135a
jsanchco <jsanchco@gmail.com>
2023-08-06 12:01:17 +0200

    Work

---
+++ src/Banking.Account.Query.Infrastructure/Migrations/20230806072012_FirstBuild.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Banking.Account.Query.Infrastructure.Migrations
+{
+    public partial class FirstBuild : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
